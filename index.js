ba1a6a35b3d6fac89d66cbe8cd0871ed43b2b106
Moya Moreno <gabriela.moyamoreno@externos-cl.cencosud.com>
2024-06-26 18:06:34 -0400

    Creating a server with Express, controllers, and routes

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import {express} from 'express'  ;
+import express from 'express';
 const app = express();
 
-//Puerto que se le puede asignar en el ambiente donde esta publicada en vivo, si no esta especificada toma el 3000
-const PORT =  process.env.PORT || 3000;
+// Rutas
+import routeIndex from './routes/routeIndex.js';
+import routeUser from './routes/routeUser.js';
 
-// Middleware for parsear 
+// Middleware para parsear JSON
 app.use(express.json());
+// Usar las rutas
+app.use('/', routeIndex);
+app.use('/users', routeUser);
 
-// Endpoint GET Raiz
-app.get('/', (req, res) => {
-  res.send('Hola, cenco!');
-});
+/* Puerto que se le puede asignar en el ambiente donde está publicada en vivo
+   Si no está especificado toma el puerto 3000*/
+   const PORT = process.env.PORT || 3000;
 
-
-
-
-// Sever
+// Inicializar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+} );
