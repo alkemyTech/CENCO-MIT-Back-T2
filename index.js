442971ff634ae634be43bf4ce6d1c9d380942b6c
Sara Rioseco <sara.rioseco@gmail.com>
2024-06-27 16:04:14 -0400

    rebase

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import express from 'express';
 import { router } from './src/routes/index.js';
 
@@ -16,3 +17,26 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 } );
+=======
+import {express} from 'express'  ;
+const app = express();
+
+//Puerto que se le puede asignar en el ambiente donde esta publicada en vivo, si no esta especificada toma el 3000
+const PORT =  process.env.PORT || 3000;
+
+// Middleware for parsear 
+app.use(express.json());
+
+// Endpoint GET Raiz
+app.get('/', (req, res) => {
+  res.send('Hola, cenco!');
+});
+
+
+
+
+// Sever
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+>>>>>>> 8622c44 (Cambios endpoint express)
