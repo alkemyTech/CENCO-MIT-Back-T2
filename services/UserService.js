5bb883ef72676bb05e0c0a2f0aa4579fb3afce9d
Silvana Vitali <silgivia@hotmail.com>
2024-06-27 17:00:38 -0400

    fix UserService

--- services/UserService.js
+++ services/UserService.js
@@ -4,10 +4,9 @@ import { UserRepository } from '../repository/index.js';
 export const UserService = {
     create: async (user) => {
         try {
-            bcrypt.hash(user.password, parseInt(process.env.SALT), (err, hash) => {
-                user.password = hash;
-                UserRepository.postUser(user);
-            });
+            const hash = await bcrypt.hash(user.password, parseInt(process.env.SALT));
+            user.password = hash;
+            await UserRepository.postUser(user);
         } catch (error) {
             console.log(error);
         }
