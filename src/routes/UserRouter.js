2f4e88c782a625a2d07cd36fc2f4de7d50b57303
Sara Rioseco <sara.rioseco@gmail.com>
2024-06-27 15:40:22 -0400

    fix files names and implemented good practices

---
+++ src/routes/UserRouter.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/UserController.js';
+
+export const UserRouter = Router();
+
+//Definition routes using controller methods
+UserRouter.get('/', UserController.getUser);
+UserRouter.post('/', UserController.createUser);
+UserRouter.put('/:id', UserController.updateUser);
+UserRouter.delete('/:id', UserController.deleteUser);
+
+export default UserRouter;
