c32df328f0e456c31924fe726fffe4a83907a617
saralopezbz <saralopezbz@gmail.com>
2024-07-01 14:36:59 -0400

    add middleware de autenticacion

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -4,11 +4,12 @@ import { isAdmin } from '../../middlewares/AuthMiddleware.js';
 import { authenticateToken } from '../middleware/auth.js';
 
 export const UserRouter = Router();
+UserRouter.get('/user', authenticateToken, UserController.getUserInfo); // Ruta protegida por el middleware de autenticación
 
-UserRouter.get('/', authenticateToken, isAdmin, UserController.getUsers);
+UserRouter.get('/', authenticateToken, isAdmin, UserController.getAllUsers);
 UserRouter.get('/user', authenticateToken, UserController.getUserInfo);
 UserRouter.get('/:id', UserController.getById);
 UserRouter.post('/', UserController.createUser);
+UserRouter.get('/:id', UserController.getById);
 UserRouter.put('/:id', UserController.updateUser);
 UserRouter.delete('/:id', UserController.deleteUser);
-
