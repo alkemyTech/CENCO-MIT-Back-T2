02964bd89cab59b3bd8f6a6d23d2adb27cd405b5
Sara Rioseco <sara.rioseco@gmail.com>
2024-07-30 14:11:09 -0400

    fix imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,7 +6,6 @@ import { User } from './entities';
 import { AuthModule } from 'src/auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
-import { IsUnique } from 'src/auth/decorators/is-unique';
 
 @Module({
   imports: [AuthModule, TypeOrmModule.forFeature([User])],
